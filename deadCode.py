a9fcf94f6412cf5503c58c01d7dd2812e4f88881
Daniel Collis <dcol5635@uni.sydney.edu.au>
2015-11-01 18:53:05 +1100

    Implemented removing instructions and refactored it a bit

--- deadCode.py
+++ deadCode.py
@@ -1,24 +1,13 @@
 def dce(cfg):
     for function in cfg.functions:
-        function = dce_function(function)
-
-    return cfg
+        dce_function(function)
 
 def dce_function(function):
-    # TODO: Make sure that the block is being updated
-    # Also make sure the correct registers are being passed
-
-    # Maybe need to save the resulted result?
-    result = dce_block(function.blocks[0])
-    print("Function Registers:", result)
-
-    return function
+    # Start from the first block
+    dce_block(function.blocks[0])
 
 def dce_block(block):
-    # TODO: Maybe somehow remove instructions if they are dead code
-    # Registers contains all live registers and last-used-in instruction
-
-    # Stores the registers at the end of this block
+    # Stores the live registers throughout the block
     registers = set()
 
     # Get the union of the registers (Rule 1)
@@ -27,24 +16,21 @@ def dce_block(block):
 
     # Go through instructions in reverse order
     for instr in block.instructions[::-1]:
-        (registers, remove) = dce_instruction(instr, registers)
+        remove = dce_instruction(instr, registers)
+        # Remove any instruction if needed
         if remove:
-            print("Remove is true", instr)
-            # TODO: somehow remove the instruction from the block
-            pass
+            block.instructions.remove(instr)
 
     return registers
 
 def dce_instruction(instr, registers):
-    # Go through each type of instruction
-    # Check if any registers need to be updated
+    # If this instruction needs to be removed (dead code)
     remove = False
 
     # Used to group instructions together and make if statements simpler
     instrGroup = {"add", "sub", "mul", "div", "lt", "gt", "eq"}
 
-    # Rule 2
-    # If being used/read
+    # Rule 2 - If being used/read
     # Instructions: st, add, sub, mul, div, lt, gt, eq, br, ret, call
     # Register becomes live (if not live already)
     if instr[0] == "st":
@@ -58,8 +44,7 @@ def dce_instruction(instr, registers):
     elif instr[0] == "call":
         registers |= {instr[3:]}
 
-    # Rule 3
-    # If being assigned and x is not being used/read
+    # Rule 3 - If being assigned and x is not being used/read
     # Instructions: lc, ld, and maybe add, sub, mul, div, lt, gt, eq, call
     # Register becomes dead (make sure that register is not used/read as well)
     if instr[0] == "lc":
@@ -81,9 +66,4 @@ def dce_instruction(instr, registers):
         if instr[1] not in {instr[3:]}:
             registers -= {instr[1]}
 
-    # Rule 4
-    # If no mention of register in instruction
-    # Register stays in same state
-    # Answer: Should be taken care of
-
-    return (registers, remove)
+    return remove
