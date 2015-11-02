581add78879d76662ee0bc38dec5e552839274fd
Daniel Collis <dancol.95@gmail.com>
2015-11-02 18:42:40 +1100

    Complete optimise.sh

--- optimise.sh
+++ optimise.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-echo "Executes the optimisation program"
+if [ ! -x "optimiser.py" ] ; then
+  chmod +x optimiser.py
+fi
 
-testFiles=$(find tests/ -type f -name '*.ir')
+if [ $# -eq 0 ] ; then
+  echo "Available optimisations:"
+  echo "    - Unreachable code"
+  echo "    - Dead code elimination"
+  echo "    - Redundant load elimination"
 
-for file in $testFiles; do
-    echo $file
-done
+  echo "For more control over the optimisation process and program, see \`./optimiser.py --help\`"
+fi
+
+if [ ! $# -eq 2 ] ; then
+  echo "'$0' requires precisely 2 arguments. For more control, run \`./optimiser.py\`"
+
+  exit 1
+else
+  ./optimiser.py -i $1 -o $2 -udr
+fi
